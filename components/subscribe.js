12d3f65ca50f256b539c19a71771b484d6248b8c
farque65 <fahim.farque@gmail.com>
2021-07-30 00:41:22 -0400

    populate email table

--- components/subscribe.js
+++ components/subscribe.js
@@ -1,45 +1,45 @@
 import { useState } from 'react'
-import { createClient } from '@supabase/supabase-js';
-
-const { SUPABASE_KEY, SUPABASE_URL } = process.env;
+import { supabase } from '../client'
 
 export default function Subscribe() {
   const [email, setEmail] = useState("")
 
-  const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
   function validEmailFormat(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    console.log('see it ', re.test(String(email).toLowerCase()));
     return re.test(String(email).toLowerCase());
   }
 
-  async function uploadData(e) {
-    const emailCurrent = e.target.value;
-    try {
-      //check table if the email is already there
-      const emailResult = await supabase
-        .from('SubscriberListEmail')
-        .select(`Email`)
-        .eq(`Email`,email)
-        .then(async (result)=>{
-          if (result.data.length < 1) {
-            const emailInsertResult = await supabase
-              .from('SubscriberListEmail')
-              .insert([
-                { Email: email }
-              ])
-          }
-          return result;
-        })
-        .catch(error=>{
-          console.log(`Error in Supabase select ${error}`);
-        })
-      setEmail("")
-      window.location.reload()
-    } catch(err) {
-      console.log('Try Catch Error ',err);
+  async function submitHandler(e) {
+    e.preventDefault();
+
+    if (!validEmailFormat(email)) {
+      return;
     }
+    
+    const { selectData, selectError } = await supabase
+      .from('emailList')
+      .select('Email')
+      .eq('Email', email)
+
+      if(selectError) {
+        throw new Error(selectError);
+      }
+
+      if (selectData && selectData.length > 0) {
+          return;
+      }
+
+    const { insertData, insertError } = await supabase
+      .from('emailList')
+      .insert([
+        { Email: email }
+    ])
+
+    if(insertError) {
+      throw new Error(insertError);
+    }
+
+    window.location.reload();
   }
 
   return (
@@ -71,21 +71,15 @@ export default function Subscribe() {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
-                onChange={(e)=> {
-                  const emailCurrent = e.target.value;
-                  if (validEmailFormat(emailCurrent)) {
-                    setEmail(emailCurrent)
-                  }
-                }}
+                onChange={(e)=> {setEmail(e.target.value)}}
               />
             </div>
           </div>
 
           <div>
             <button
-              type="submit"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={uploadData}
+              onClick={submitHandler}
             >
               Submit
             </button>
